6bc0a40d62a625178d22b3f561f2a36d21d0ed18
Abdalrahman Valabji <valabji@Abdalrahmans-MacBook-Pro.local>
2022-10-16 22:59:09 +0200

    impleminting husky

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement } from "react";
 import '../style/components/footer.scss';
 export default function footer(): ReactElement {
   return <div className="footer">Copy rights saved © Zumar 2022</div>;
